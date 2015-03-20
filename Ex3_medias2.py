0b3f55cdab2ff0e78d00babfc2820490ab36662f
Rodrigotanure <rodrigottricarico@gmail.com>
2015-03-20 11:24:13 -0300

    Create Ex3_medias2.py

---
+++ Ex3_medias2.py
@@ -0,0 +1,14 @@
+notas=[]
+x=0
+v=True
+while v:
+    nota=float(raw_input('Digite a nota: \n'))
+    if nota>0:
+        notas.append(nota)
+        y=len(notas)
+        x+=nota
+        print(y)
+        print x
+    else:
+        print ('Sua media e: %f')%(x/y)
+        break
